fcbf8af5c4984e13f7221c6d3d50a8c0d4b04a50
titan <damiankomonski@gmail.com>
2025-04-26 20:58:09 +0200

    html

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,64 @@
-// Import our custom CSS
 import '../scss/styles.scss';
-
-// Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap';
+import Swiper from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 1000,
+  },
+  modules: [Autoplay],
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    480: {
+      slidesPerView: 3,
+    },
+    800: {
+      slidesPerView: 4,
+    },
+    1024: {
+      slidesPerView: 5,
+    },
+    1200: {
+      slidesPerView: 6
+    }
+  },
+});
+
+const swiper_opinions = new Swiper('.swiper-opinions', {
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 1000,
+  },
+  modules: [Navigation, Autoplay],
+  navigation: {
+    nextEl: '.swiper-opinions-prev',
+    prevEl: '.swiper-opinions-next',
+  },
+  spaceBetween: 30,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 1,
+    },
+    800: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 3
+    }
+  },
+});
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default {
   build: {
     outDir: '../dist'
   },
+  base: "./",
   server: {
     port: 8080
   },
